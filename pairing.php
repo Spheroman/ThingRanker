b5f8a9052129b7a0a52051aa7141e1345b121cd9
Spheroman <jackwen04@gmail.com>
2024-05-16 16:49:11 +0800

    start.php

--- pairing.php
+++ pairing.php
@@ -11,9 +11,6 @@ class pairing
     public int $winner; //I think ID would be best here, but 1/2 could work as well. bool in that case
 
     //TODO: get a pairing from 1 of 3 options: random, rating based, and reliability.
-    //for rating based, pair them against ones with close ratings, but still random
-    //for reliability, have 1 be a very low reliability and have 2 be random. this will quickly stabilize things added mid comp
-    //maybe split the pairing generation into a separate function, so we can
     function __construct(string $id, int $method)
     {
         $this->id = $id;
---
+++ start.php
@@ -0,0 +1,3 @@
+<?php
+// hidden from users
+//TODO: update table to mark as started, create match history table
