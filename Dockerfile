73af369c5a6955c709a973d1f65ef7d5481391f0
Spheroman <80714339+Spheroman@users.noreply.github.com>
2024-06-06 00:46:58 +0800

    improved start sh

--- Dockerfile
+++ Dockerfile
@@ -16,5 +16,6 @@ COPY . /var/www/html
 
 RUN a2enmod rewrite
 RUN docker-php-ext-install pdo pdo_mysql
+RUN --host=0.0.0.0
 
 USER www-data
---
+++ start.sh
@@ -0,0 +1,36 @@
+
+if ! [ -x "$(command -v docker compose)" ];
+  then
+    user="$(id -un 2>/dev/null || true)"
+    sh_c='sh -c'
+    if [ "$user" != 'root' ]; then
+      if command_exists sudo; then
+        sh_c='sudo -E sh -c'
+      else
+        echo Error: needs sudo privledges to run install script
+        exit 1
+      fi
+    fi
+    $sh_c apt-get update
+    $sh_c apt-get install ca-certificates curl
+    $sh_c install -m 0755 -d /etc/apt/keyrings
+    $sh_c curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    $sh_c chmod a+r /etc/apt/keyrings/docker.asc
+    $sh_c echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      tee /etc/apt/sources.list.d/docker.list > /dev/null
+    $sh_c apt-get update
+    $sh_c apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin git
+fi
+
+if ! [ -e ./ThingRanker/compose.yaml ]
+  then
+  $sh_c git fetch "https://github.com/Spheroman/ThingRanker.git"
+  $sh_c cd ThingRanker
+  $sh_c git checkout docker
+  else
+    $sh_c cd ThingRanker
+fi
+
+docker compose up
