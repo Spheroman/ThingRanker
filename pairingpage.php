e56cdf41b70da8099cddc837257abe1dff7e5bc2
Spheroman <80714339+Spheroman@users.noreply.github.com>
2024-06-06 22:28:27 +0800

    minor changes

--- pairingpage.php
+++ pairingpage.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Setup</title>
-        <link href="/pairing.css" rel="stylesheet">
-    </head>
-
-    <body>
-
+<head>
 <?php
 session_start();
 require "pairing.php";
@@ -24,25 +18,31 @@ $id = $_GET["id"];
 $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if(!isset($_SESSION["uuid"])){
+if (!isset($_SESSION["uuid"])) {
     $_SESSION["uuid"] = generateRandomString(8);
 }
-if(!isset($_SESSION["pairing" . $id])) {
-    if(!tableCheck($id, $pdo)) die("comp not found");
-    if(!startedCheck($id, $pdo)) die("comp has not started");
+if (!isset($_SESSION["pairing" . $id])) {
+    if (!tableCheck($id, $pdo)) die("comp not found");
+    if (!startedCheck($id, $pdo)) die("comp has not started");
     $curr = Pairing::fromRandom($id, $pdo);
 } else $curr = unserialize($_SESSION["pairing" . $id]);
-if($curr->iscomplete){
+if ($curr->iscomplete) {
     $curr = Pairing::fromRandom($id, $pdo);
 }
 
 $stmt = $pdo->prepare("SELECT name FROM comps WHERE id = :id");
 $stmt->bindParam(":id", $id);
 $stmt->execute();
-$name = $stmt->fetch(PDO::FETCH_ASSOC)['name'];
+$name = htmlspecialchars($stmt->fetch(PDO::FETCH_ASSOC)['name']);
+echo "<title>$name</title>";
+
+echo '<link href="/pairing.css" rel="stylesheet">
+    </head>
+    <body>
+';
 echo "<a href='/$id'><h1>$name</h1></a>";
 
-$_SESSION["pairing". $id] = serialize($curr);
+$_SESSION["pairing" . $id] = serialize($curr);
 $p1 = $curr->p1;
 $p2 = $curr->p2;
 
