ee467fb03ddc3cd5f05ca5b62423ae0738dba755
Spheroman <80714339+Spheroman@users.noreply.github.com>
2024-06-09 21:58:29 +0800

    setup might do something

--- setup.sh
+++ setup.sh
@@ -1,8 +1,42 @@
-sudo apt update
-sudo apt-get install -y apache2 php mariadb-server git php-mysql
+apt update
+apt-get install -y apache2 php mariadb-server git php-mysql
 a2enmod rewrite
-git clone https://github.com/Spheroman/ThingRanker.git
-ln -s ThingRanker /var/www/html
+rm -rf /var/www/html
+git clone https://github.com/Spheroman/ThingRanker.git /var/www/html
+read -p "New Database Root Password: " ROOTPASS
+read -p "ThingRanker Database User Password: " USERPASS
 PHPINI=$(php -i | grep /.+/php.ini -oE)
 echo -e "extension=pdo.so\nextension=pdo_mysql.so" >> "$PHPINI"
+mysql -e "ALTER USER root@localhost IDENTIFIED BY '$ROOTPASS'"
+mysql -e "DROP USER ''@'localhost'"
+mysql -e "DROP USER ''@'$(hostname)'"
+mysql -e "CREATE USER 'ThingRanker'@'localhost' IDENTIFIED WITH mysql_native_password BY '$USERPASS'"
+mysql -e "DROP DATABASE IF EXISTS test"
+mysql -e "CREATE DATABASE ThingRanker"
+mysql -e "GRANT PRIVILEGE ON ThingRanker.* TO 'ThingRanker'@'localhost'"
+mysql -e "USE DATABASE ThingRanker";
+mysql -e "create table comps
+(
+    id          char(6)                    not null,
+    name        varchar(100)               not null,
+    time        datetime   default (now()) not null,
+    updated     datetime   default (now()) null on update CURRENT_TIMESTAMP,
+    started     tinyint(1) default 0       null,
+    passcode    char(60)                   null,
+    publicadd   tinyint(1) default 0       not null,
+    addwhilerun tinyint(1) default 0       not null,
+    playerlimit smallint   default -1      not null,
+    pairingtype tinyint    default 0       not null,
+    maxrounds   smallint   default -1      not null
+);"
+mysql -e "FLUSH PRIVILEGES";
+
+rm -f /var/www/html/config.php
+echo -e "<?php
+\n
+const DB_HOST = 'localhost';\n
+const DB_USER = 'ThingRanker';\n
+const DB_PASS = '$USERPASS';\n
+const DB_NAME = 'ThingRanker';\n" >> /var/www/html/config.php
+
 systemctl restart apache2
