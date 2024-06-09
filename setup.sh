34205409b41d2e075a8ece3d7552bd56036d74bc
Spheroman <80714339+Spheroman@users.noreply.github.com>
2024-06-09 21:37:28 +0800

    setup might do something

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+sudo apt update
+sudo apt-get install -y apache2 php mariadb-server git php-mysql
+a2enmod rewrite
 git clone https://github.com/Spheroman/ThingRanker.git
 ln -s ThingRanker /var/www/html
+PHPINI=$(php -i | grep /.+/php.ini -oE)
+echo -e "extension=pdo.so\nextension=pdo_mysql.so" >> "$PHPINI"
+systemctl restart apache2
